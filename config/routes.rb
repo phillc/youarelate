29ffdfd29009bba2555b00eb3f4e889ef9a1276f
phillc <spyyderz@gmail.com>
2009-08-21 22:29:10 -0400

    moved around templates

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ ActionController::Routing::Routes.draw do |map|
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
 
-  map.root :controller => "users"
+  map.root :controller => :welcome
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
