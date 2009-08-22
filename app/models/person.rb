b481b7c130c0e23f1bc28287f73acc16459754a4
phillc <spyyderz@gmail.com>
2009-08-22 02:59:02 -0400

    pulling data from stubs right now

--- app/models/person.rb
+++ app/models/person.rb
@@ -14,4 +14,17 @@
 class Person < ActiveRecord::Base
   belongs_to :user
   has_many :datas
+  
+  # stub for now
+  # TODO: delete this
+  def notes
+    <<-eos
+      Lorem ipsum dolor sit amet, consectetur magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur.  Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum
+    eos
+  end
 end
