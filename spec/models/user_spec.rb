26e28edc488400c542c533e6899c1ae53eaed00c
phillc <spyyderz@gmail.com>
2009-08-22 05:25:09 -0400

    rspec plugins fine now, writing a few before bed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe User do
-  it "should be invalid empty" do
+  it "should be invalid when empty" do
+    User.new.save.should == false
   end
 end
