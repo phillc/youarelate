df3907d051440be61f187d38c9d504a8c99ba7b2
phillc <spyyderz@gmail.com>
2009-08-21 23:54:38 -0400

    prepping the dashboard page for some serious business

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
   def show
     @user = User.find(params[:id])
+    #@people = user.people
+    #a simple stub:
+    @people = %w{ Harry John Mark Stacey Jacob Matt Richard }.collect{|name| Person.new(:name => name)}
   end
 
 end
