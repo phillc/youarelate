b2dea2e239b695ea396ccad9b436b546e8acdf20
Hubert Liu <hubert.c.liu@gmail.com>
2009-08-21 23:44:50 -0400

    login view changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :require_user
+  before_filter :require_user, :except => [:new, :create]
 
   # GET /users/new
   # GET /users/new.xml
