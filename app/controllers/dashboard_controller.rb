cf4cdab3177cb9dda08d86d1aa2c66c060f8ae16
phillc <spyyderz@gmail.com>
2009-08-22 03:46:26 -0400

    stubbing stats

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,10 @@
 class DashboardController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @people = @user.people
-    
     #TODO: remove this
     #a simple stub:
-    if @people.empty?
+    if @user.people.empty?
       %w{ Harry John Mark Stacey Jacob Matt Richard }.each{|name| @user.people.create(:name => name)}
-      @people = @user.people
     end
   end
 
--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,10 @@
 class StatsController < ApplicationController
   def index
-  end
+    @user = User.find(params[:dashboard_id])
+    @people = @user.people.find(params[:people])
 
+    # TODO: stubbing
+    @invite_time = DateTime.now
+    @invite_probability = rand(30) + 70
+  end
 end
