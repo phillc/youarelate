e562651df028068bc5390a0d369383d74d7570ea
phillc <spyyderz@gmail.com>
2009-08-22 22:34:47 -0400

    Default user data, removed it from controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,11 +4,6 @@ class DashboardController < ApplicationController
   before_filter :require_equal_user
   
   def show
-    #TODO: remove this
-    #a simple stub:
-    if @user.people.empty?
-      %w{ Harry John Mark Stacey Jacob Matt Richard }.each{|name| @user.people.create(:name => name)}
-    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,54 @@ class User < ActiveRecord::Base
 
 
   def populate_starter_data
-
+    #TODO: change this
+    #
+    [
+      {
+        :name => "Drew",
+        :notes => "This mother fucker is always late.",
+      },
+      {
+        :name => "Harry",
+        :notes => "Heh, I bet his variance is a little crazy",
+      },
+      {
+        :name => "John",
+        :notes => "Rofl, what can I say about this strange man?",
+      },
+      {
+        :name => "Mark",
+        :notes => "Well, I once saw him going 100 mph... how can he ever be late like that?",
+      },
+      {
+        :name => "Stacey",
+        :notes => "If she wasn't hot, we would have big problems",
+      },
+      {
+        :name => "Christy",
+        :notes => "She isn't hot. We have big problems.",
+      },
+      {
+        :name => "Matt",
+        :notes => "I guess he could... remote in...",
+      },
+      {
+        :name => "Richard",
+        :notes => "last time he was on time was back when he was still sucking his mother's nip",
+      }
+    ].each do |peep|
+      n = rand(12)
+      n.times do
+        pick = rand(4)
+        case pick
+          when 0..2
+            self.people.build.data_points.build(:expected_time => DateTime.now - rand(50).days - rand(24).hours - rand(60).minutes)
+          when 3
+            self.people.build.data_points.build(:time_difference => rand(100) - 50)
+        end
+      end
+    end
+                               
   end
 
 
